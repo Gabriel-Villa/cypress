c61f6fe53aeb81a428b358a8d90f00b35cc9b662
Gabriel-Villa <VillaCapurroGabriel@gmail.com>
2024-07-27 21:42:14 -0500

    Add nvmrc and install cypress

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+// import { defineConfig } from "cypress";
+
+// export default defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
+
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+    component: {
+        devServer: {
+            framework: "vue",
+            bundler: "vite",
+        },
+    },
+});
